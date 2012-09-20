4742299f8a3f2621f818da505ceee7fa1f8d71cd
root <root@ibm-m4-srv1.cs.bgu.ac.il>
2012-09-20 19:48:48 +0300

    attributes for version 0.9.10

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-maintainer       "Heavy Water Software Inc."
-maintainer_email "ops@hw-ops.com"
+maintainer       "Piavlo"
+maintainer_email "lolitushka@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures graphite"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.1"
+version          "0.3.2"
 
 supports "ubuntu"
 
